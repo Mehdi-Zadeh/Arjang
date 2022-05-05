7f72fcd7fe3d62c3cb53613801b1a7abe2e18176
DaBigM <mahdi.shokri@gmail.com>
2022-05-05 12:55:21 +0430

    Second Session

---
+++ Session 2/basics.py
@@ -0,0 +1,77 @@
+import math
+
+
+print('bah bah' 'che classi' 'che ostadi')
+print('bah bah', 'che classi', 'che ostadi')
+print('bah bah' + 'che classi' + 'che ostadi')
+
+print(2 + 3)
+print(2 * 3)
+print(2 / 3)
+print(2 // 3)
+print(2 % 3)
+print(2 << 3)
+print(2 < 3)
+
+print('=' * 40)  # print khoob
+
+a = 2
+b = 3.5
+
+my_beautiful_int = 6
+myBeautifulInt = 6   # camel case
+
+# python data types
+
+# numeric, sequence, mapping, file, class, instance, exception
+
+# numeric:
+# P. V. 2: int, long, float, complex
+# P. V. 3: int, float, complex
+
+# print(math.factorial(10000))
+
+# sequence:
+# list, tuple, string, range, iterator
+# ------------
+# binary sequence: bytes, bytearray, memoryview
+
+# mapping:
+# dictionary, set
+
+# bool: True, False
+
+# ===================================
+
+alphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+print(alphabetString[3])  # indexing
+print(alphabetString[2: 6])  # slicing
+print(alphabetString[: 6])
+print(alphabetString[2:])
+print(alphabetString[2: 18: 2])
+print(alphabetString[::])
+print(alphabetString[-1])
+print(alphabetString[19])
+print(alphabetString[-7])
+print(alphabetString[1])
+print(alphabetString[-1: 5: -1])
+print(alphabetString[5: 40000])
+
+print('=' * 40)
+
+print(alphabetString[-1::-1])  # zangian
+print(alphabetString[-1 :0 :-1])  # olapour
+print(alphabetString[-1:])  # babaei
+print(alphabetString[-1: 32: -1])  # hashemi
+print(alphabetString[-1:])  # parsa
+print(alphabetString[-1::-1])  # mollaei
+print(alphabetString[-1: : -1])  # kheirandish
+print(alphabetString[-1: 26: -1])  # hashemi version 2
+
+# ===================================
+print('=' * 40)
+print(alphabetString[::-1])
+
+
+
---
+++ Session 2/string_formatting.py
@@ -0,0 +1,26 @@
+
+sampleString = 'this is a sample string \twithout line breaks'
+
+anotherString = 'hello, my name is Mehdi, I am 18 :D'
+anotherAnotherString = "hello, my name's Mehdi, I am 18 :D"
+
+bahbah = '''salam halet chetore?
+vaghean oomadim khate badi
+bah bah che pythoni'''
+
+print(sampleString)
+print(bahbah)
+
+name = 'Mehdi'
+age = 18
+
+print('hello, my name is Mehdi, I am 18 :D')
+print('hello, my name is %s, I am %d :D' % (name, age))
+print('hello, my name is', name, 'I am', age, ':D')
+
+print('hello, my name is {1}, I am {0} :D'.format(age, name))
+print(1'hello, my name is {name}, I am {age} :D')
+
+# jalase badi edamasho begoo : yadet nare X
+
+
